ea20b2b8fd487c96dba3a1f81bfdab45bc39ceb1
Selman Jir CAN <cselmanjir@gmail.com>
2022-04-06 13:47:24 +0300

    Projenin mvc altyapısının kurulması #2
    Projeye gerekli paketlerin yüklenmesi ve mvc altyapısının oluşturulması

---
+++ src/migrations/20220307055650-create-users.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      full_name: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
